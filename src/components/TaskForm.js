018675c0d4f204e4b3f72e1b884e1e0d9f00fd2b
NguyenNgocLam <lamnn8@fpt.com.vn>
2019-10-26 11:09:13 +0700

    update

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -2,79 +2,16 @@ import React from 'react';
 import {actionAddTask} from './../actions/index'
 import {connect} from 'react-redux'
 class TaskForm extends React.Component {
-  constructor(props){
-    super(props) ;
-     this.state = {
-       id:'',
-       name:'',
-       status:false
-     }
-    
-  }
-  onCloseFor = ()=>{
-    this.props.onCloseForm()
-  }
- 
-  handleChange  = (e)=>{
-      let  target = e.target ;
-      let  name = target.name ;
-      let  value = target.value ;
-       if(name==='status'){
-        value = target.value === 'true'? true : false
-    }
-      this.setState({
-         [name]:value
-      })
-  }
-  
-  handleSubmit = (e)=>{
-    e.preventDefault()
-    // this.props.onSubmit(this.state)
-    console.log(this.state)
-    this.props.Submit(this.state)
-    this.onCloseFor()
-  }
-  CloseForm = ()=>{
-    this.setState({
-      name:'',
-      status:false
-    })
-    this.props.onCloseForm()
-  }
-  
-  // componentWillMount = ()=>{
-  //   if(this.props.handleEditing){
-  //      this.setState({
-  //        id: this.props.handleEditing.id,
-  //        name: this.props.handleEditing.name,
-  //        status: this.props.handleEditing.status
-  //      })
-  //   }
-      
-  // }
-
-  // componentWillReceiveProps = nextProps =>{
-  //    if(nextProps && nextProps.handleEditing){
-  //     this.setState({
-  //       id: nextProps.id,
-  //       name: nextProps.name,
-  //       status: nextProps.status
-  //     })
-  //    }
-  // }
-componentWillUpdate = ()=>{
-    const  {task} = this.props
-    console.log(task)
-  }
+   
   render() {
 
     return (
        
             <div className="panel panel-warning">
               <div className="panel-title">
-                <h3>   {this.props.editing ?  'them cong viec' : 'update cong viec' }
+                <h3>    them cong viec 
                 <span className="fa fa-times-circle text-right" 
-                onClick={this.onCloseFor}
+               
                 
                 ></span></h3>
               </div>
@@ -86,16 +23,14 @@ componentWillUpdate = ()=>{
                       type="text"
                       className="form-control"
                       name="name"
-                      value={this.state.name}
-                      onChange = {this.handleChange}
+                      
                     />
                   </div>
                   <label>trang thai:</label>
                   <select
                     className="form-control"
                     name="status"
-                    value={this.state.status}
-                    onChange = {this.handleChange}
+                   
                   >
                     <option value={true}>Kick hoat</option>
                     <option value={false}>an</option>
@@ -117,18 +52,5 @@ componentWillUpdate = ()=>{
     )
   }
 }
-const mapStateToProps = state=>{
-  console.log(state)
-  return{
-    task:state.ReducerTaskList
-  }
-}
-const mapDispatchProps = dispath=>{
-  return {
-    Submit:(task)=>{
-       
-      dispath(actionAddTask(task))
-  }
-}
-}
-export default connect(mapStateToProps,mapDispatchProps)(TaskForm)
+ 
+export default  (TaskForm)
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -2,37 +2,33 @@ import React from 'react';
  
 class TaskItem extends React.Component {
  
-  handleStatus = ()=>{
-    this.props.handleStatus(this.props.task.id)
-  }
-  handleDelete =()=>{
-    this.props.handleDelete(this.props.task.id)
-  }
-  handleUpdate= ()=>{
-    this.props.handleUpdate(this.props.task.id)
-  }
+ 
   render() {
-      const {task,index} = this.props
+    let task = {
+      id:1,
+      name:'ff',
+      status:1
+    }
     return (
         <>
           <tr>
-            <td>{index}</td>
+            <td>{task.id}</td>
             <td>{task.name}</td>
             <td className="text-center">
-              <button className={ task.status ? "btn btn-primary" : "btn btn-warning" }type="button" 
-             onClick={this.handleStatus}
+              <button className=  "btn btn-warning" type="button" 
+            
              >
-                 <span className="fa fa-danger mr-5">{ task.status ? "kick hoat" :"an"}</span>
+                 <span className="fa fa-danger mr-5"> kick hoat</span>
               </button>
              
             </td>
             <td className="text-center">
-              <button className="btn btn-danger" onClick={this.handleUpdate}> 
+              <button className="btn btn-danger"  > 
               <span className="fa fa-pencil mr-5"></span>sua
               </button>
               &nbsp;
 
-              <button type="button" className="btn btn-danger" onClick={this.handleDelete}>
+              <button type="button" className="btn btn-danger" >
                 <span className="fa fa-trash mr-5"></span>xoa
               </button>
              
